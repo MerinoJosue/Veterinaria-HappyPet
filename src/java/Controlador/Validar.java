6ccf4d33aea57132abb0af4d3c58916b12a6f318
Josue Merino <>
2024-05-17 12:56:14 -0500

    ajustes

--- src/java/Controlador/Validar.java
+++ src/java/Controlador/Validar.java
@@ -32,7 +32,7 @@ public class Validar extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String accion = request.getParameter("accion");
         if (accion.equalsIgnoreCase("Ingresar")) {
